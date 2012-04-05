cedd77f3a64edb3c0cbc60ffa91d7bd96bd73aa4
tntuan0712494@gmail.com <tntuan0712494@gmail.com@90ca5d87-4b36-e6ce-69fe-318ea44fb5e8>
2012-04-05 06:04:36 +0000

    [new project] add control dynamic not user control

---
+++ trunk/SourceCode/SilverlightBookDemo2/Paris.Controls/Dock.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+namespace Paris.Controls
+{
+    public enum Dock : byte
+    {
+        Bottom = 3,
+        Left = 0,
+        Right = 1,
+        Top = 2
+    }
+
+    public enum PageFoldAction
+    {
+        TurnPageOnClick,
+        TurnPageOnDoubleClick,
+        None
+    }
+
+
+
+    public enum PageFoldVisibility
+    {
+        OnMouseOver,
+        Never,
+        Always
+    }
+
+ 
+
+
+    public enum BookZone
+    {
+        Out,
+        BottomLeft,
+        TopLeft,
+        Center,
+        TopRight,
+        BottomRight
+    }
+
+ 
+
+}
---
+++ trunk/SourceCode/SilverlightBookDemo2/SilverlightBookDemo2/MainPage.xaml.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using System.Windows.Media.Imaging;
+
+namespace SilverlightBookDemo2
+{
+    public partial class MainPage : UserControl
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            this.Loaded += new RoutedEventHandler(MainPage_Loaded);
+        }
+
+        void MainPage_Loaded(object sender, RoutedEventArgs e)
+        {
+            //ImageSource ism = new ImageSource();
+            //imgCover.Source = new BitmapImage(new Uri(@"http://www.rajneeshnoonia.com/blog/uploads/images/header.png", UriKind.Absolute));
+
+            //Button bt = new Button();
+            //bt.Content = "asdasd";
+            //bt.Width = 100;
+            //bt.Height = 25;
+            //bt.Click += new RoutedEventHandler(bt_Click);
+            //slbook1.Items.Add(bt);
+
+            try
+            {
+                WebClient wc = new WebClient();
+                wc.OpenReadCompleted += wc_OpenReadCompleted;
+                wc.OpenReadAsync(new Uri(@"\Data.xml", UriKind.RelativeOrAbsolute));
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        void bt_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("bt_Click");
+        }
+    
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Button_Click");
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void wc_OpenReadCompleted(object sender, OpenReadCompletedEventArgs e)
+        {
+            try
+            {
+                //Kiem tra thuoc tinh Error cho cac loi 
+                if (e.Error != null)
+                {
+                    return;
+                }
+                //Neu khong co loi, Lay du lieu stream ve va phan tich chung toi XDocument thong qua phuong thuc Load 
+                using (System.IO.Stream s = e.Result)
+                {
+                    //UIElement eles = Utils.LoadXamlFromStream(s);
+                    //slbook1.Items.Clear();
+
+                    //slbook1.Items.Add(eles);
+
+                    //Button bt = new Button();
+                    //bt.Content = "asdasd";
+                    //bt.Width = 100;
+                    //bt.Height = 25;
+                    //bt.Click += new RoutedEventHandler(bt_Click);
+                    //slbook1.Items.Add(bt);
+
+                    slbook1.Items.Clear();
+                    List<UIElement> list = Utils.LoadXamlFromStream(s);
+                    for (int i = 0; i < list.Count; i++)
+                    {
+                        slbook1.Items.Add(list[i]);
+                    }
+                    slbook1.InvalidateArrange();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        } 
+    }
+}
