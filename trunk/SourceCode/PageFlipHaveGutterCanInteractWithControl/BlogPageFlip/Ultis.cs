a73f3df66e9622f8555f2947dc70900c784a237a
tntuan0712494@gmail.com <tntuan0712494@gmail.com@90ca5d87-4b36-e6ce-69fe-318ea44fb5e8>
2012-04-30 10:24:26 +0000

    project that intergrate gutter book pixel shader

---
+++ trunk/SourceCode/PageFlipHaveGutterCanInteractWithControl/BlogPageFlip/Ultis.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+namespace PageFlipUltis
+{
+    public static class Ultis
+    {
+        //public static UIElement cloneElement(UIElement orig)
+        //{
+
+        //    if (orig == null)
+
+        //        return (null);
+
+        //    string s = XamlWriter.Save(orig);
+
+        //    StringReader stringReader = new StringReader(s);
+
+        //    XmlReader xmlReader = XmlTextReader.Create(stringReader, new XmlReaderSettings());
+
+        //    return (UIElement)XamlReader.Load(xmlReader);
+
+        //}
+
+        ////dll reference to System.Runtime.Serialization
+        //public static T DeepCopy<T>(this T oSource)
+        //{
+        //    T oClone;
+
+        //    System.Runtime.Serialization.DataContractSerializer dcs = new System.Runtime.Serialization.DataContractSerializer(typeof(T));
+
+        //    using (System.IO.MemoryStream ms = new System.IO.MemoryStream())
+        //    {
+        //        dcs.WriteObject(ms, oSource);
+        //        ms.Position = 0;
+        //        oClone = (T)dcs.ReadObject(ms);
+        //    }
+
+        //    return oClone;
+        //}
+
+        public static UIElement LoadXamlFromString(string xaml)
+        {
+                //            XDocument doc = XDocument.Load(s);
+                //OutputTextBlock.Text = doc.ToString(SaveOptions.OmitDuplicateNamespaces);
+                //string xaml = doc.ToString(SaveOptions.OmitDuplicateNamespaces);
+
+                return System.Windows.Markup.XamlReader.Load(xaml) as UIElement;
+        }
+    }
+}
