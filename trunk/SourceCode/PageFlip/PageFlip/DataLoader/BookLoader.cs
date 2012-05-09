7d24fe2d34c6415ebb853a5197aa580bca3d4b88
tntuan0712494@gmail.com <tntuan0712494@gmail.com@90ca5d87-4b36-e6ce-69fe-318ea44fb5e8>
2012-05-09 07:41:58 +0000

    [feature] ChangeChapter [bug] next article page

--- trunk/SourceCode/PageFlip/PageFlip/DataLoader/BookLoader.cs
+++ trunk/SourceCode/PageFlip/PageFlip/DataLoader/BookLoader.cs
@@ -16,10 +16,25 @@ namespace PageFlip.DataLoader
     {
         public string ArticleName;
         public int Index;
+        public int ParentIndex;
+        public UIElement CreateArticlePage(int iIndex)
+        {
+            Button bt = new Button();
+            bt.Width = 200;
+            bt.Height = 25;
+            bt.Content = "Chapter " + this.ParentIndex.ToString() + " Article " + this.Index.ToString() + " Page " + iIndex.ToString();
+            return bt;
+        }
+
+        public void AddParent(Chapter chr)
+        {
+            this.ParentIndex = chr.Index;
+        }
     }
+
     public class Chapter
     {
-        List<Article> listArticle;
+        public List<Article> Articles;
         public int Index;
 
         public UIElement CreateChapterFromArticleName()
@@ -34,41 +49,65 @@ namespace PageFlip.DataLoader
         public static Chapter LoadChapter(int iNextChapterIndex)
         {
             Chapter chp = new Chapter();
+            chp.Index = iNextChapterIndex;
+
             List<Article> list = new List<Article>();
             for (int i = 0; i < 5; i++)
             {
                 Article ar = new Article();
                 ar.ArticleName = "ArticleName" + i.ToString();
                 ar.Index = i;
+                ar.AddParent(chp);
 
                 list.Add(ar);
             }
 
-            chp.listArticle = list;
-            chp.Index = iNextChapterIndex;
+            chp.Articles = list;
+            
             return chp;
         }
     }
+
     public class BookLoader
     {
         List<Chapter> listChapter;
+        //int CurrentChapterIndex;
+        //int CurrentArticleIndex;
+        //int CurrentArticlePageIndex;
+
+        public UIElement CurrentPage;
+        public UIElement NextPageLeftPart;
+        public UIElement NextPageRightPart;
 
         public BookLoader()
         {
             listChapter = new List<Chapter>();
+                //load all chapter infomation
+                for (int i = 0; i < 3; i++)
+                {
+                    Chapter nextChap = Chapter.LoadChapter(i);
+                    listChapter.Add(nextChap);
+                }
         }
 
-        public UIElement ChangeChapter (int iCurrentChapterIndex, int iNextChapterIndex)
+        public void UpdateBeforeChangeChapter(int iNextChapterIndex)
         {
-            Chapter nextChap = Chapter.LoadChapter(iNextChapterIndex);
-            return nextChap.CreateChapterFromArticleName();
+            NextPageLeftPart = listChapter[iNextChapterIndex].CreateChapterFromArticleName();
+            NextPageRightPart = listChapter[iNextChapterIndex].CreateChapterFromArticleName();
         }
 
+        public void UpdateAfterChangeChapter(int iChapterIndex)
+        {
+            //load 1 current page ad 2 more next page
+            CurrentPage = listChapter[iChapterIndex].CreateChapterFromArticleName();
+            NextPageLeftPart = listChapter[iChapterIndex].Articles[0].CreateArticlePage(0);
+            NextPageRightPart = listChapter[iChapterIndex].Articles[0].CreateArticlePage(0);
+        }
         
-        public void toArticle(int iChapterIndex, int iArticleIndex);//animation RToL//return article first page
+//        public void toArticle(int iChapterIndex, int iArticleIndex);//animation RToL//return article first page
 
-//change 1 article page
-        public void nextArticlePage(int iArticleIndex, int iCurrentPageIndex);// can return a article page or next chapter page
-        public void previousArticlePage(int iArticleIndex, int iCurrentPageIndex);// can return a article page or current chapter page
+////change 1 article page
+//        public void nextArticlePage(int iArticleIndex, int iCurrentPageIndex);// can return a article page or next chapter page
+//        public void previousArticlePage(int iArticleIndex, int iCurrentPageIndex);// can return a article page or current chapter page
     }
 }
