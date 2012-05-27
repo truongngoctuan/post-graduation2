92e84f76e8799dac171f7f1e5f20db04e08c9a3f
tntuan0712494 <tntuan0712494@gmail.com>
2012-05-27 08:11:25 +0000

    [feature] add button Tile [bug] cant click on button [bug] add only one page but still can transition page if move cursor to conner [bug] only add menu lvl 0 with1 page, NEED to update code read xml to parse better

---
+++ SourceCode/PageFlip/PageFlip/DataLoader/IObserver.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using System.Collections.Generic;
+
+namespace PageFlip.DataLoader
+{
+
+    /// <summary>
+    /// The 'Subject' abstract class
+    /// </summary>
+    public abstract class Subject
+    {
+        private List<IObserver> _observers = new List<IObserver>();
+
+        public void Attach(IObserver observer)
+        {
+            _observers.Add(observer);
+        }
+
+        public void Detach(IObserver observer)
+        {
+            _observers.Remove(observer);
+        }
+
+        public void Notify()
+        {
+            foreach (IObserver o in _observers)
+            {
+                o.UpdateInterface();
+            }
+        }
+    }
+
+    /// <summary>
+    /// The 'Observer' abstract class
+    /// </summary>
+    public interface IObserver
+    {
+        void UpdateInterface();
+    }
+
+}
