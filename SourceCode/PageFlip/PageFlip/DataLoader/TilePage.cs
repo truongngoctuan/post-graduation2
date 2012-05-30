1c803d37c8ece8365f5239be0df306d4804c4736
tntuan0712494 <tntuan0712494@gmail.com>
2012-05-30 02:05:34 +0000

    dynamic load grid data

--- SourceCode/PageFlip/PageFlip/DataLoader/TilePage.cs
+++ SourceCode/PageFlip/PageFlip/DataLoader/TilePage.cs
@@ -15,18 +15,22 @@ using System.IO;
 namespace PageFlip.DataLoader
 {
    
-    public class TilePage
+    public abstract class TilePage
     {//generate a page from multiple tiles
         //tiles in this case mean chapter thumnails
         public virtual UIElement generatePage(int iPage)
         {
             return new Button() { Width = 100, Height = 25, Content = "Test Tile Page " +iPage.ToString() };
         }
+
+        public abstract void InitParams(List<object> Params);
     }
     
     public class TilePageMenu : TilePage
     {
         public List<Tile> Tiles;
+        public int NGridRows;
+        public int NGridColumns;
 
         public TilePageMenu()
         {
@@ -35,27 +39,52 @@ namespace PageFlip.DataLoader
 
         public override UIElement generatePage(int iPage)
         {
-            string xaml = @"
-	<Grid 
+//            string xaml = @"
+//	<Grid 
+//xmlns='http://schemas.microsoft.com/client/2007'
+//    xmlns:x='http://schemas.microsoft.com/winfx/2006/xaml'
+//x:Name='tilepage'>
+//        <Grid.ColumnDefinitions>
+//            <ColumnDefinition Width='*'></ColumnDefinition>
+//            <ColumnDefinition Width='*'></ColumnDefinition>
+//            <ColumnDefinition Width='*'></ColumnDefinition>
+//            <ColumnDefinition Width='*'></ColumnDefinition>
+//        </Grid.ColumnDefinitions>
+//        <Grid.RowDefinitions>
+//            <RowDefinition Height='*'></RowDefinition>
+//            <RowDefinition Height='*'></RowDefinition>
+//            <RowDefinition Height='*'></RowDefinition>
+//            <RowDefinition Height='*'></RowDefinition>
+//            <RowDefinition Height='*'></RowDefinition>
+//        </Grid.RowDefinitions>
+//    </Grid>
+//
+//";
+            string xamlColumns = string.Empty;
+            for (int i = 0; i < NGridColumns; i++)
+            {
+                xamlColumns += "<ColumnDefinition Width='*'></ColumnDefinition>\r\n";
+            }
+
+            string xamlRows = string.Empty;
+            for (int i = 0; i < NGridRows; i++)
+            {
+                xamlRows += "<RowDefinition Height='*'></RowDefinition>\r\n";
+            }
+
+            string xaml = string.Format(@"
+<Grid 
 xmlns='http://schemas.microsoft.com/client/2007'
     xmlns:x='http://schemas.microsoft.com/winfx/2006/xaml'
 x:Name='tilepage'>
         <Grid.ColumnDefinitions>
-            <ColumnDefinition Width='*'></ColumnDefinition>
-            <ColumnDefinition Width='*'></ColumnDefinition>
-            <ColumnDefinition Width='*'></ColumnDefinition>
-            <ColumnDefinition Width='*'></ColumnDefinition>
+            {0}
         </Grid.ColumnDefinitions>
         <Grid.RowDefinitions>
-            <RowDefinition Height='*'></RowDefinition>
-            <RowDefinition Height='*'></RowDefinition>
-            <RowDefinition Height='*'></RowDefinition>
-            <RowDefinition Height='*'></RowDefinition>
-            <RowDefinition Height='*'></RowDefinition>
+            {1}
         </Grid.RowDefinitions>
     </Grid>
-
-";
+", xamlColumns, xamlRows);
 
             int iCounter = 0;
             Grid grd = (Grid)PageFlipUltis.Ultis.LoadXamlFromString(xaml);
@@ -72,6 +101,12 @@ x:Name='tilepage'>
                 return grd;
             else
                 return null;
+        }
+
+        public override void InitParams(List<object> Params)
+        {
+            NGridRows = int.Parse(Params[0].ToString());
+            NGridColumns = int.Parse(Params[1].ToString());
         }
         
     }
